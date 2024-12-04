b9967f248f5b5ed25728ee9353a8dde27bef5149
Nathaël Chéry <107258483+NathaelC@users.noreply.github.com>
2024-12-04 23:39:15 +0100

    prise en charge de la mise à jour des cartons

--- static/js/affichage.js
+++ static/js/affichage.js
@@ -4,7 +4,14 @@ function updateDisplay() {
     $.post(`/infos/${id_arene}`, function(data) {
         $(`#score_rouge`).text(data.arene["score"]['rouge']);
         $(`#score_vert`).text(data.arene["score"]['vert']);
+        
         // TODO : ajouter le suivi des cartons
+        $(`#cbt_rouge_carton_blanc`).text(data.arene['cartons']['rouge']['blanc']);
+        $(`#cbt_rouge_carton_jaune`).text(data.arene['cartons']['rouge']['jaune']);
+        $(`#cbt_rouge_carton_rouge`).text(data.arene['cartons']['rouge']['rouge']);
+        $(`#cbt_vert_carton_blanc`).text(data.arene['cartons']['vert']['blanc']);
+        $(`#cbt_vert_carton_jaune`).text(data.arene['cartons']['vert']['jaune']);
+        $(`#cbt_vert_carton_rouge`).text(data.arene['cartons']['vert']['rouge']);
     });
 }
 
